10d37e55980f89aa2fcd99bc844323060905b4f2
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-06-09 23:38:45 -0700

    Add terrain avoidance.

--- naval-ai.lua
+++ naval-ai.lua
@@ -13,6 +13,10 @@ EscapeDrive = 1
 
 AllowedAngleError = 2
 
+MinDepth = 10
+LookAheadTimes = { 1, 5, 10 }
+LookAheadAngles = { -90, -45, -15, 0, 15, 45, 90 }
+
 -- API constants
 WATER = 0
 LAND = 1
@@ -36,7 +40,7 @@ Roll = 0
 TargetInfo = nil
 
 function GetSelfInfo(I)
-   Position = I:GetConstructCenterOfMass()
+   Position = I:GetConstructPosition()
 
    Yaw = I:GetConstructYaw()
 
@@ -67,8 +71,47 @@ function sign(n)
       return 0
    end
 end
-      
-function SetHeading(I)
+
+function AvoidTerrain(I, angle)
+   -- For now, we scan in front rather than take actual velocity into account
+   local offset = Vector3(0, 0, I:GetConstructMaxDimensions().z)
+   local velocity = I:GetVelocityMagnitude()
+   local count,avoid = 0,Vector3.zero
+   for i,t in pairs(LookAheadTimes) do
+      for j,a in pairs(LookAheadAngles) do
+         local pos = offset + Vector3.forward * t * velocity
+         pos = Quaternion.Euler(0, a, 0) * pos
+         if I:GetTerrainAltitudeForLocalPosition(pos) > -MinDepth then
+            count = count + 1
+            avoid = avoid - pos
+         end
+      end
+   end
+
+   --I:LogToHud("count "..count.." avoid "..tostring(avoid))
+
+   if count == 0 then
+      return angle
+   else
+      -- To world coordinates
+      avoid = Position + Quaternion.Euler(0, Yaw, 0) * avoid
+      -- Vector3.Angle not working?
+      return -I:GetTargetPositionInfoForPosition(0, avoid.x, 0, avoid.z).Azimuth
+   end
+end
+
+function SetHeading(I, angle)
+   angle = AvoidTerrain(I, angle)
+   if math.abs(angle) > AllowedAngleError then
+      if angle > 0 then
+         I:RequestControl(WATER, YAWRIGHT, 1)
+      else
+         I:RequestControl(WATER, YAWLEFT, 1)
+      end
+   end
+end
+
+function SetHeadingToTarget(I)
    local distance = TargetInfo.GroundDistance
    local azimuth = -TargetInfo.Azimuth
    --I:LogToHud("Distance "..distance.." Azimuth "..azimuth)
@@ -92,13 +135,7 @@ function SetHeading(I)
 
    --I:LogToHud("State "..state.." Drive "..drive.." TargetAngle "..targetAngle)
 
-   if math.abs(targetAngle) > AllowedAngleError then
-      if targetAngle > 0 then
-         I:RequestControl(WATER, YAWRIGHT, 1)
-      else
-         I:RequestControl(WATER, YAWLEFT, 1)
-      end
-   end
+   SetHeading(I, targetAngle)
 
    return drive
 end
@@ -107,7 +144,7 @@ function SetSpeed(I, drive)
    I:RequestControl(WATER, MAINPROPULSION, drive)
 end
 
-function SetTarget(I)
+function GetTarget(I)
    TargetInfo = nil
    for mindex = 0,I:GetNumberOfMainframes()-1 do
       for tindex = 0,I:GetNumberOfTargets(mindex)-1 do
@@ -120,12 +157,12 @@ end
 
 function Update(I)
    if I.AIMode == 'combat' then
-      --GetSelfInfo(I)
+      GetSelfInfo(I)
 
       local drive = 0
-      if SetTarget(I) then
+      if GetTarget(I) then
          I:TellAiThatWeAreTakingControl()
-         drive = SetHeading(I)
+         drive = SetHeadingToTarget(I)
          SetSpeed(I, drive)
       end
    end
