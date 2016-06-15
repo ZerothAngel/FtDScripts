40cde8ef098c5b25163c18b54819e26b7648b255
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-06-15 15:54:34 -0700

    Break away from parent and repair other friendlies if they are close enough and/or damaged enough.

--- main/repair-ai-header.lua
+++ main/repair-ai-header.lua
@@ -3,14 +3,38 @@
 -- Set to true to control ship when AI set to "on" as well
 ActivateWhenOn = false
 
--- Offset from parent. Note the Y value is ignored.
-ParentOffset = Vector3(25, 0, 25)
-ParentMaxDistance = 25
--- Throttle when distance from parent is >ParentMaxDistance
+-- Offset from repair target's center of mass.
+-- Note the Y value is ignored.
+RepairTargetOffset = Vector3(25, 0, 25)
+ApproachMaxDistance = 25
+-- Throttle when distance from repair target is >ApproachMaxDistance
 ClosingDrive = 1
--- Throttle when within ParentMaxDistance
+-- Throttle when within ApproachMaxDistance
 -- Probably not a good idea for hydrofoil-based subs to stop
-LoiterDrive = 0.2
+LoiterDrive = 0.1
+
+-- When considering other repair targets, they must
+-- be within this distance and within this altitude range
+RepairTargetMaxDistance = 1000
+RepairTargetMaxParentDistance = 1000
+RepairTargetMinAltitude = -10
+RepairTargetMaxAltitude = 25
+-- And have health equal to or below this fraction
+RepairTargetMaxHealthFraction = 0.95
+-- And have health equal to or above this fraction
+RepairTargetMinHealthFraction = 0.25
+
+-- Repair targets are scored according to
+-- Distance * DistanceWeight + ParentDistance * ParentDistanceWeight +
+--   Damage * DamageWeight
+-- Where Distance is this ship's distance from the target
+-- ParentDistance is the target's distance from the parent
+-- Damage is 1.0 - HealthFraction
+DistanceWeight = 0
+ParentDistanceWeight = -0.02
+DamageWeight = 100.0
+-- Parent's score multiplied by this bonus (or penalty if <1)
+ParentBonus = 1.1
 
 -- Yaw PID controller settings
 -- These default values have worked well for me on
--- main/repair-ai.lua
+++ main/repair-ai.lua
@@ -1,5 +1,5 @@
 --! repair-ai
---@ avoidance commons pid
+--@ avoidance commons pid spairs
 YawPID = PID.create(YawPIDValues[1], YawPIDValues[2], YawPIDValues[3], -1.0, 1.0)
 
 FirstRun = nil
@@ -8,20 +8,7 @@ Origin = nil
 TargetInfo = nil
 
 ParentID = nil
-
-function Imprint(I)
-   ParentID = nil
-   local Closest = math.huge
-   for i = 0,I:GetFriendlyCount()-1 do
-      local Friend = I:GetFriendlyInfo(i)
-      local Offset,_ = PlanarVector(CoM, Friend.CenterOfMass)
-      local Distance = Offset.magnitude
-      if Distance < Closest then
-         Closest = Distance
-         ParentID = Friend.Id
-      end
-   end
-end
+RepairTargetID = nil
 
 -- Called on first activation (not necessarily first Update)
 function FirstRun(I)
@@ -62,26 +49,31 @@ function AdjustHeadingToPoint(I, Point)
    AdjustHeading(I, -I:GetTargetPositionInfoForPosition(0, Point.x, 0, Point.z).Azimuth)
 end
 
-function AdjustHeadingToParent(I)
+function AdjustHeadingToRepairTarget(I)
+   local __func__ = "AdjustHeadingToRepairTarget"
+
    local Drive = LoiterDrive
-   local Parent = I:GetFriendlyInfoById(ParentID)
-   if Parent and Parent.Valid then
-      local ParentCoM = Parent.CenterOfMass + ParentOffset
-      local Offset,_ = PlanarVector(CoM, ParentCoM)
+   local RepairTarget = I:GetFriendlyInfoById(RepairTargetID)
+   if RepairTarget and RepairTarget.Valid then
+      if Debugging then Debug(I, __func__, "RepairTarget %s", RepairTarget.BlueprintName) end
+
+      local RepairTargetCoM = RepairTarget.CenterOfMass + RepairTargetOffset
+      local Offset,_ = PlanarVector(CoM, RepairTargetCoM)
       local Distance = Offset.magnitude
       local Direction = Offset / Distance
       -- Not so sure about this intercept formula, since it will tend
       -- to stay parallel with parent if we don't cap InterceptTime
-      local RelativeVelocity = I:GetVelocityVector() - Parent.Velocity
+      local RelativeVelocity = I:GetVelocityVector() - RepairTarget.Velocity
       local RelativeSpeed = Vector3.Dot(RelativeVelocity, Direction)
-      local InterceptTime = 0
+      local InterceptTime = 1
       if RelativeSpeed > 0.0 then
          InterceptTime = Distance / RelativeSpeed
          InterceptTime = math.min(InterceptTime, 10)
+         InterceptTime = math.max(InterceptTime, 1)
       end
 
-      local TargetPoint = ParentCoM + Parent.Velocity * InterceptTime
-      if Distance > ParentMaxDistance then
+      local TargetPoint = RepairTargetCoM + RepairTarget.Velocity * InterceptTime
+      if Distance > ApproachMaxDistance then
          AdjustHeadingToPoint(I, TargetPoint)
          Drive = ClosingDrive
       end
@@ -89,9 +81,89 @@ function AdjustHeadingToParent(I)
    return Drive
 end
 
+function CalculateRepairTargetWeight(I, Distance, ParentDistance, Friend)
+   return Distance * DistanceWeight +
+      ParentDistance * ParentDistanceWeight +
+      (1.0 - Friend.HealthFraction) * DamageWeight
+end
+
+function SelectRepairTarget(I)
+   local __func__ = "SelectRepairTarget"
+
+   -- Get Parent info
+   local Parent = I:GetFriendlyInfoById(ParentID)
+   if Parent and not Parent.Valid then
+      -- Hmm, parent gone (taken out of play?)
+      -- Skip for now, select new parent next update
+      RepairTargetID = nil
+      -- And ensure we latch onto a new parent
+      ParentID = nil
+      return
+   end
+
+   local Targets = {}
+   -- Parent first, adjust weight accordingly
+   local ParentCoM = Parent.CenterOfMass
+   local Offset,_ = PlanarVector(CoM, ParentCoM)
+   Targets[Parent.Id] = CalculateRepairTargetWeight(I, Offset.magnitude, 0, Parent) * ParentBonus
+
+   -- Scan nearby friendlies
+   for i = 0,I:GetFriendlyCount()-1 do
+      local Friend = I:GetFriendlyInfo(i)
+      if Friend and Friend.Valid and Friend.Id ~= ParentID then
+         -- Meets range and altitude requirements?
+         local FriendCoM = Friend.CenterOfMass
+         local FriendAlt = FriendCoM.y
+         local FriendHealth = Friend.HealthFraction
+         Offset,_ = PlanarVector(CoM, FriendCoM)
+         local Distance = Offset.magnitude
+         Offset,_ = PlanarVector(ParentCoM, FriendCoM)
+         local ParentDistance = Offset.magnitude
+         if Distance <= RepairTargetMaxDistance and
+            ParentDistance <= RepairTargetMaxParentDistance and
+            FriendAlt >= RepairTargetMinAltitude and
+            FriendAlt <= RepairTargetMaxAltitude and
+            FriendHealth <= RepairTargetMaxHealthFraction and
+            FriendHealth >= RepairTargetMinHealthFraction then
+            Targets[Friend.Id] = CalculateRepairTargetWeight(I, Distance, ParentDistance, Friend)
+         end
+      end
+   end
+
+   if Debugging then
+      local NumTargets = 0
+      -- Huh. # operator only works on sequences
+      for k in pairs(Targets) do NumTargets = NumTargets+1 end
+      Debug(I, __func__, "#Targets %d", NumTargets)
+   end
+
+   -- Sort
+   for k,v in spairs(Targets, function(t,a,b) return t[b] < t[a] end) do
+      RepairTargetID = k
+      -- Only care about the first one
+      break
+   end
+end
+
 -- Sets throttle
 function SetDriveFraction(I, Drive)
    I:RequestControl(WATER, MAINPROPULSION, Drive)
+end
+
+function Imprint(I)
+   ParentID = nil
+   local Closest = math.huge
+   for i = 0,I:GetFriendlyCount()-1 do
+      local Friend = I:GetFriendlyInfo(i)
+      if Friend and Friend.Valid then
+         local Offset,_ = PlanarVector(CoM, Friend.CenterOfMass)
+         local Distance = Offset.magnitude
+         if Distance < Closest then
+            Closest = Distance
+            ParentID = Friend.Id
+         end
+      end
+   end
 end
 
 -- Finds first valid target on first mainframe
@@ -122,7 +194,10 @@ function Update(I)
             Imprint(I)
          end
          if ParentID then
-            Drive = AdjustHeadingToParent(I)
+            SelectRepairTarget(I)
+         end
+         if RepairTargetID then
+            Drive = AdjustHeadingToRepairTarget(I)
          end
       else
          ParentID = nil
