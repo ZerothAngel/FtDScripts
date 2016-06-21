9710446553d1817ec29107179286e3acfb6e6c73
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-06-21 10:45:25 -0700

    Add simple main script to test avoidance module.

---
+++ main/avoidancetest-header.lua
@@ -0,0 +1,4 @@
+-- CONFIGURATION
+
+-- Set to true to control ship when AI set to "on" as well
+ActivateWhenOn = false
---
+++ main/avoidancetest.lua
@@ -0,0 +1,26 @@
+--! avoidancetest
+--@ yawthrottle avoidance commons
+Mode = WATER
+
+FirstRun = nil
+
+function FirstRun(I)
+   FirstRun = nil
+
+   AvoidanceFirstRun(I)
+end
+
+function Update(I)
+   local AIMode = I.AIMode
+   if (ActivateWhenOn and AIMode == 'on') or AIMode == 'combat' then
+      GetSelfInfo(I)
+
+      if FirstRun then FirstRun(I) end
+
+      -- Suppress default AI
+      if AIMode == 'combat' then I:TellAiThatWeAreTakingControl() end
+
+      -- Just go as straight as possible
+      AdjustHeading(I, 0)
+   end
+end
