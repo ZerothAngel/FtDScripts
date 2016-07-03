339ec19a427f7379ede4d46bf927972b3b625514
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-03 10:18:27 -0700

    YawThrottle module directly depends on Avoidance... which may need to be fixed in the future.

--- lib/yawthrottle.lua
+++ lib/yawthrottle.lua
@@ -1,4 +1,4 @@
---@ commons pid spinnercontrol
+--@ avoidance commons pid spinnercontrol
 -- Yaw & throttle module
 YawPID = PID.create(YawPIDValues[1], YawPIDValues[2], YawPIDValues[3], -1.0, 1.0, UpdateRate)
 
