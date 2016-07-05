4761c63dd5faee478656a5ac3901193404fb3346
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-05 00:44:38 -0700

    Add maximum enemy range setting.

--- lib/gettargetpositioninfo.lua
+++ lib/gettargetpositioninfo.lua
@@ -6,7 +6,7 @@ function GetTargetPositionInfo(I)
    for mindex = 0,I:GetNumberOfMainframes()-1 do
       for tindex = 0,I:GetNumberOfTargets(mindex)-1 do
          TargetPositionInfo = I:GetTargetPositionInfo(mindex, tindex)
-         if TargetPositionInfo.Valid then return true end
+         if TargetPositionInfo.Valid and TargetPositionInfo.Range <= MaxEnemyRange then return true end
       end
    end
    TargetPositionInfo = nil
