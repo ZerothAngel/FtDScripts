0364c4077c8832f54bdd1b3ba3501b2c43d815a2
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-09 16:42:26 -0700

    Add Javelin variant of mixed missiles script.

---
+++ main/mixedmissilesjav-header.lua
@@ -0,0 +1,7 @@
+-- CONFIGURATION
+
+-- Minimum target altitude for missiles
+MissileMinAltitude = -25
+
+-- Maximum target altitude for torpedoes
+TorpedoMaxAltitude = 15
---
+++ main/mixedmissilesjav.lua
@@ -0,0 +1,44 @@
+--! mixedmissilesjav
+--@ missiledriver javelinmissile bottomattacktorpedo periodic
+-- Mixed missiles main
+MyMissile = JavelinMissile.create()
+MyTorpedo = BottomAttackTorpedo.create()
+
+GuidanceInfos = {
+   {
+      Controller = MyMissile,
+      CanTarget = function (I, TargetInfo) return TargetInfo.Position.y >= MissileMinAltitude end
+   },
+   {
+      Controller = MyTorpedo,
+      CanTarget = function (I, TargetInfo) return TargetInfo.Position.y <= TorpedoMaxAltitude end
+   }
+}
+
+function IsVertical(Info)
+   -- NB If perpendicular, it's usually never exactly 0
+   return math.abs(Vector3.Dot(Info.LocalForwards, Vector3.up)) > 0.001
+end
+
+-- Returns index into GuidanceInfos
+function SelectGuidance(I, BlockInfo)
+   -- Really simple. Vertical launcher = missile, horizontal = torpedo
+   if IsVertical(BlockInfo) then
+      return 1
+   else
+      return 2
+   end
+end
+
+-- Main update loop
+function MissileMain_Update(I)
+   if not I:IsDocked() then
+      MissileDriver_Update(I, GuidanceInfos, SelectGuidance)
+   end
+end
+
+MissileMain = Periodic.create(UpdateRate, MissileMain_Update)
+
+function Update(I)
+   MissileMain:Tick(I)
+end
