5930ca2af1eafaa8b09b7e2e65c487c5454a61dd
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-09 17:34:42 -0700

    Make JavelinMissile look like the other missile controllers.

--- missile/javelinmissile-header.lua
+++ missile/javelinmissile-header.lua
@@ -1,3 +1,5 @@
+-- JAVELIN SETTINGS
+
 -- Only perform top-attack for targets below this altitude.
 -- If above this altitude, intercept as normal.
 JavelinAirTargetAltitude = 10
@@ -9,6 +11,12 @@ JavelinClosingHeight = 100
 -- The maximum distance (on the ground) to travel toward the target
 -- when attempting to reach the cruising height. Smaller = steeper ascent.
 JavelinClosingDistance = 50
+
+-- Evasion settings while closing
+-- First number is magnitude of evasion in meters (to each side)
+-- Second number is time scale, smaller is slower. <1 recommended.
+-- Set whole thing to nil to disable, e.g. JavelinEvasion = nil
+JavelinEvasion = { 20, .25 }
 
 -- Within this ground distance, the missile will follow a simple
 -- intercept course. Smaller = steeper descent. Make sure your
--- missile/javelinmissile.lua
+++ missile/javelinmissile.lua
@@ -9,34 +9,48 @@ function JavelinMissile.create()
    return self
 end
 
--- JavelinMissile instance methods
-
-function JavelinMissile:SetTarget(I, TargetPosition, TargetAimPoint, TargetVelocity, TargetInfo)
-   self.TargetGround = math.max(I:GetTerrainAltitudeForPosition(TargetPosition), 0)
-   self.DoTopAttack = (TargetPosition.y - self.TargetGround) <= JavelinAirTargetAltitude
-end
-
-function JavelinMissile:Guide(I, TransceiverIndex, MissileIndex, TargetPosition, TargetAimPoint, TargetVelocity, Missile)
-   -- Calculate standard aim point
-   local MissilePosition = Missile.Position
-   local MissileVelocity = Missile.Velocity
-   local AimPoint = QuadraticIntercept(MissilePosition, MissileVelocity, TargetAimPoint, TargetVelocity)
-
-   -- Then modify according to ground distance from target/launcher
-   local NewTarget = Vector3(AimPoint.x, MissilePosition.y, AimPoint.z)
-   local GroundOffset = NewTarget - MissilePosition
+function JavelinMissile.TopAttack(I, Position, Velocity, AimPoint, TargetGround, Time, Offset)
+   local NewTarget = Vector3(AimPoint.x, Position.y, AimPoint.z)
+   local GroundOffset = NewTarget - Position
    local GroundDistance = GroundOffset.magnitude
 
-   if GroundDistance < JavelinTerminalDistance or not self.DoTopAttack then
+   if GroundDistance < TerminalDistance then
       -- Always return real aim point when within terminal distance
       return AimPoint
    else
       -- Closing
       local GroundDirection = GroundOffset / GroundDistance
-      local NewAimPoint = MissilePosition + GroundDirection * JavelinClosingDistance
+      local NewAimPoint = Position + GroundDirection * JavelinClosingDistance
       -- TODO Hmmm... this is almost the same as BottomAttackTorpedo but without the
       -- terrain logic.
-      NewAimPoint.y = self.TargetGround + JavelinClosingHeight
+      NewAimPoint.y = TargetGround + JavelinClosingHeight
+      if JavelinEvasion then
+         local Perp = Vector3.Cross(GroundDirection, Vector3.up)
+         NewAimPoint = NewAimPoint + Perp * JavelinEvasion[1] * (2 * Mathf.PerlinNoise(JavelinEvasion[2] * Time, Offset) - 1)
+      end
       return NewAimPoint
    end
+end
+
+-- JavelinMissile instance methods
+
+function JavelinMissile:SetTarget(I, TargetPosition, TargetAimPoint, TargetVelocity, TargetInfo)
+   self.Time = I:GetTimeSinceSpawn()
+   self.TargetGround = math.max(I:GetTerrainAltitudeForPosition(TargetPosition), 0)
+   self.DoTopAttack = (TargetPosition.y - self.TargetGround) <= JavelinAirTargetAltitude
+end
+
+function JavelinMissile:Guide(I, TransceiverIndex, MissileIndex, TargetPosition, TargetAimPoint, TargetVelocity, Missile)
+   local MissilePosition = Missile.Position
+   local MissileVelocity = Missile.Velocity
+   local AimPoint = QuadraticIntercept(MissilePosition, MissileVelocity, TargetAimPoint, TargetVelocity)
+
+   if self.DoTopAttack then
+      local Offset = TransceiverIndex * 37 + MissileIndex
+      AimPoint = JavelinMissile.TopAttack(I, MissilePosition, MissileVelocity,
+                                          AimPoint, self.TargetGround,
+                                          self.Time, Offset)
+   end
+
+   return AimPoint
 end
