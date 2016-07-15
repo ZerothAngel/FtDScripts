313046bf5f32694a8390a4759a49552ecd89f4e3
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-15 14:55:04 -0700

    Make sure GetSelfInfo is called when it's included.

--- check.sh
+++ check.sh
@@ -15,10 +15,20 @@ check_firstrun () {
   return 0
 }
 
+check_getselfinfo() {
+  if fgrep Modules: "$1" | fgrep getselfinfo >/dev/null; then
+    if ! egrep '^\W*GetSelfInfo' "$1" >/dev/null; then
+      echo "missing call to GetSelfInfo"
+      return 1
+    fi
+  fi
+  return 0
+}
+
 for f in out/*.lua; do
   if lua52 -l dummy "$f"; then
     echo -n "$f: "
-    if check_firstrun "$f"; then
+    if check_firstrun "$f" && check_getselfinfo "$f"; then
       echo "ok"
     fi
   fi
