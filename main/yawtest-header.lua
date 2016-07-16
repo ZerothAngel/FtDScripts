cc1fdaedd0160059b82f56905016205e1152742c
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-15 21:45:06 -0700

    yawtest: Output turn duration and turn radius.

--- main/yawtest-header.lua
+++ main/yawtest-header.lua
@@ -3,6 +3,9 @@
 -- Set to true to control ship when AI set to "on" as well
 ActivateWhenOn = false
 
+VaryThrottle = true
+ThrottleSettings = { 1, .5 }
+
 -- How often to run. At 1, it will run every update. At 10,
 -- it will run every 10th update. The lower it is, the more
 -- responsive it will be, but it will also take more processing time.
--- main/yawtest.lua
+++ main/yawtest.lua
@@ -1,6 +1,9 @@
 --! yawtest
 --@ yawthrottle getselfinfo firstrun periodic
 MyHeading = -90
+LastTurn = nil
+
+ThrottleIndex = 0
 
 function YawTest_Update(I)
    YawThrottle_Reset()
@@ -8,6 +11,12 @@ function YawTest_Update(I)
    MyHeading = (MyHeading + 90) % 360
    I:LogToHud(string.format("New heading! %f degrees", MyHeading))
    SetHeading(MyHeading)
+   if VaryThrottle then
+      ThrottleIndex = ThrottleIndex + 1
+      SetThrottle(ThrottleSettings[ThrottleIndex])
+      ThrottleIndex = ThrottleIndex % #ThrottleSettings
+   end
+   LastTurn = { I:GetTimeSinceSpawn(), CoM }
 end
 
 YawTest = Periodic.create(UpdateRate, YawTest_Update)
@@ -18,6 +27,15 @@ function Update(I)
       GetSelfInfo(I)
 
       if FirstRun then FirstRun(I) end
+
+      if LastTurn and Mathf.DeltaAngle(Yaw, MyHeading) < 0.1 then
+         local DeltaTime = I:GetTimeSinceSpawn() - LastTurn[1]
+         local Distance = (CoM - LastTurn[2]).magnitude
+         local Message = string.format("Time: %.2f s, Distance: %.2f m", DeltaTime, Distance)
+         I:Log(Message)
+         I:LogToHud(Message)
+         LastTurn = nil
+      end
 
       YawTest:Tick(I)
 
