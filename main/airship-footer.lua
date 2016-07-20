8dc45299e03076590bde6d118948c648a58d9f4e
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-19 20:33:02 -0700

    Move Mode configuration to main scripts.

---
+++ main/airship-footer.lua
@@ -0,0 +1,4 @@
+-- Secret configurables to turn it into a cheap 2D-only aviation AI
+Mode = AIR
+AttackRuns = true
+ForceAttackTime = 30
---
+++ main/warship-footer.lua
@@ -0,0 +1,4 @@
+-- Secret configurables to turn it into a cheap 2D-only aviation AI
+Mode = WATER
+AttackRuns = false
+ForceAttackTime = 15
