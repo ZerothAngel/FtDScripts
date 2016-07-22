87aa82b38354f05f2052328768f6fc5ac271f684
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-07-21 17:35:50 -0700

    stabilizer: Fix upwards-facing thrusters, rescale PIDs.

--- control/stabilizer-header.lua
+++ control/stabilizer-header.lua
@@ -3,12 +3,12 @@
 -- PID values for stabilization using propulsion elements.
 -- Start with { 1, 0, 0 } and tune from there.
 RollPIDConfig = {
-   Kp = .1,
+   Kp = .5,
    Ti = 5,
    Td = .1,
 }
 PitchPIDConfig = {
-   Kp = .1,
+   Kp = .5,
    Ti = 5,
    Td = .1,
 }
--- control/stabilizer.lua
+++ control/stabilizer.lua
@@ -1,23 +1,10 @@
 --@ api sign pid
 -- Stabilizer module
-RollPID = PID.create(RollPIDConfig, -1, 1)
-PitchPID = PID.create(PitchPIDConfig, -1, 1)
+RollPID = PID.create(RollPIDConfig, -10, 10)
+PitchPID = PID.create(PitchPIDConfig, -10, 10)
 
 LastPropulsionCount = 0
 PropulsionInfos = {}
-
--- TODO Refactor. Most of the following ripped from subcontrol.
-
--- Returns 1, -1, or 0 depending on whether or not propulsion provides
--- thrust along the Y axis.
-function GetPropulsionSign(BlockInfo)
-   local DotY = BlockInfo.LocalForwards.y
-   if math.abs(DotY) > 0.001 then
-      return Mathf.Sign(DotY)
-   else
-      return 0
-   end
-end
 
 -- Determine sign and location of propulsion elements
 function ClassifyPropulsion(I)
@@ -29,15 +16,15 @@ function ClassifyPropulsion(I)
 
       for i = 0,PropulsionCount-1 do
          local BlockInfo = I:Component_GetBlockInfo(PROPULSION, i)
-         local LocalSign = GetPropulsionSign(BlockInfo)
-         if LocalSign ~= 0 then
+         local LocalForwards = BlockInfo.LocalForwards
+         if math.abs(LocalForwards.y) > .001 then
             -- Only care about propulsion pointing up or down
             local CoMOffset = BlockInfo.LocalPositionRelativeToCom
+            local UpSign = Sign(LocalForwards.y)
             local Info = {
                Index = i,
-               LocalSign = LocalSign,
-               RollSign = ControlRoll and Sign(CoMOffset.x) or 0,
-               PitchSign = ControlPitch and Sign(CoMOffset.z) or 0,
+               RollSign = ControlRoll and (Sign(CoMOffset.x) * UpSign) or 0,
+               PitchSign = ControlPitch and (Sign(CoMOffset.z) * UpSign) or 0,
             }
             table.insert(PropulsionInfos, Info)
          end
@@ -63,13 +50,9 @@ function Stabilizer_Update(I)
          local RollSign,PitchSign = Info.RollSign,Info.PitchSign
          if RollSign ~= 0 or PitchSign ~= 0 then
             -- Sum up inputs and constrain
-            local Output = RollCV * RollSign + PitchCV * PitchSign
-            Output = math.max(0, math.min(1, Output))
-            -- If pointing the other way, set output to 0
-            if (Output * Info.LocalSign) < 0 then
-               Output = 0
-            end
-            I:Component_SetFloatLogic(PROPULSION, Info.Index, Output)
+            local Output = math.max(0, RollCV * RollSign) + math.max(0, PitchCV * PitchSign)
+            Output = math.min(10, Output)
+            I:Component_SetFloatLogic(PROPULSION, Info.Index, Output / 10)
          end
       end
    end
