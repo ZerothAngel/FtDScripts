7f33f055b1bded9668164ccd48fadf3db990e41a
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-09-03 09:26:58 -0700

    Add quadcopter version of repair AI.

---
+++ main/repairquad-header.lua
@@ -0,0 +1,14 @@
+-- CONFIGURATION
+
+-- Activate on these AI modes. Valid keys are "off", "on", "combat",
+-- "patrol", and "fleetmove".
+ActivateWhen = {
+--   on = true,
+   combat = true,
+}
+
+-- How often to run. At 1, it will run every update. At 10,
+-- it will run every 10th update. The lower it is, the more
+-- responsive it will be, but it will also take more processing time.
+AI_UpdateRate = 4
+Quadcopter_UpdateRate = 4
---
+++ main/repairquad.lua
@@ -0,0 +1,30 @@
+--! repairquad
+--@ getselfinfo firstrun periodic
+--@ threedofspinner altitudecontrol repair-ai
+-- Quadcopter repair AI
+ThreeDoFSpinner = Periodic.create(Quadcopter_UpdateRate, Altitude_Control)
+RepairAI = Periodic.create(AI_UpdateRate, RepairAI_Update)
+
+function Update(I)
+   if not I:IsDocked() then
+      GetSelfInfo(I)
+
+      if FirstRun then FirstRun(I) end
+
+      ThreeDoFSpinner:Tick(I)
+
+      if ActivateWhen[I.AIMode] then
+         RepairAI:Tick(I)
+
+         -- Suppress default AI
+         I:TellAiThatWeAreTakingControl()
+
+         YawThrottle_Update(I)
+      else
+         ParentID = nil
+         RepairTargetID = nil
+      end
+
+      ThreeDoFSpinner_Update(I)
+   end
+end
