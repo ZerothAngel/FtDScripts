acaef066b838d70e924c8e19dd7346696caeea2f
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-10-07 20:37:05 -0700

    Experimental directional script for launch of interceptors.

--- lib/getweaponcontrollers.lua
+++ lib/getweaponcontrollers.lua
@@ -7,13 +7,17 @@ TURRET = 4
 MISSILECONTROL = 5
 FIRECONTROLCOMPUTER = 6
 
-function AddWeaponInfo(Weapons, WeaponInfo, WeaponType)
+function AddWeaponInfo(Weapons, WeaponInfo, WeaponType, TurretIndex, WeaponIndex)
    if WeaponType and WeaponInfo.WeaponType ~= WeaponType then
       return
    else
+      local WeaponSlot = WeaponInfo.WeaponSlot
+      local OnTurret = TurretIndex ~= nil
       local Weapon = {
          Position = WeaponInfo.GlobalPosition,
-         Slot = WeaponInfo.WeaponSlot,
+         Slot = WeaponSlot,
+         TurretIndex = TurretIndex,
+         Index = WeaponIndex,
       }
       table.insert(Weapons, Weapon)
    end
@@ -23,12 +27,12 @@ function GetWeaponControllers(I, WeaponType)
    local Weapons = {}
    for i = 0,I:GetWeaponCount()-1 do
       local Info = I:GetWeaponInfo(i)
-      AddWeaponInfo(Weapons, Info, WeaponType)
+      AddWeaponInfo(Weapons, Info, WeaponType, nil, i)
    end
    for i = 0,I:GetTurretSpinnerCount()-1 do
       for j = 0,I:GetWeaponCountOnTurretOrSpinner(i)-1 do
          local Info = I:GetWeaponInfoOnTurretOrSpinner(i, j)
-         AddWeaponInfo(Weapons, Info, WeaponType)
+         AddWeaponInfo(Weapons, Info, WeaponType, i, j)
       end
    end
    return Weapons
---
+++ main/directionalintercept.lua
@@ -0,0 +1,101 @@
+--! directionalintercept
+--@ getweaponcontrollers periodic getselfinfo
+function GetWeaponsForSlot(Controllers, WeaponSlot)
+   local Weapons = {}
+
+   for _,Controller in pairs(Controllers) do
+      if Controller.Slot == WeaponSlot then
+         table.insert(Weapons, Controller)
+      end
+   end
+
+   return Weapons
+end
+
+function DirectionalIntercept_Update(I)
+   local ToGlobal = Quaternion.LookRotation(I:GetConstructForwardVector(), I:GetConstructUpVector())
+   local ToLocal = Quaternion.Inverse(ToGlobal)
+
+   local ToFire = {}
+
+   for mindex=0,I:GetNumberOfMainframes()-1 do
+      for windex=0,I:GetNumberOfWarnings(mindex)-1 do
+         local Missile = I:GetMissileWarning(mindex, windex)
+         if Missile.Valid and Missile.Range <= InterceptRange then
+            local MissilePosition = Missile.Position
+            local Offset = MissilePosition - CoM
+            -- Convert to local
+            local LocalOffset = ToLocal * Offset
+            -- Mark table accordingly
+            if LocalOffset.x < 0 then
+               if LocalOffset.y < 0 then
+                  if LocalOffset.z < 0 then
+                     ToFire.LeftLowerRear = MissilePosition
+                  else
+                     ToFire.LeftLowerForward = MissilePosition
+                  end
+               else
+                  if LocalOffset.z < 0 then
+                     ToFire.LeftUpperRear = MissilePosition
+                  else
+                     ToFire.LeftUpperForward = MissilePosition
+                  end
+               end
+            else
+               if LocalOffset.y < 0 then
+                  if LocalOffset.z < 0 then
+                     ToFire.RightLowerRear = MissilePosition
+                  else
+                     ToFire.RightLowerForward = MissilePosition
+                  end
+               else
+                  if LocalOffset.z < 0 then
+                     ToFire.RightUpperRear = MissilePosition
+                  else
+                     ToFire.RightUpperForward = MissilePosition
+                  end
+               end
+            end
+         end
+      end
+   end
+
+   local Controllers = GetWeaponControllers(I, MISSILECONTROL)
+   local WeaponsForSlot = {}
+
+   for Octant,MissilePosition in pairs(ToFire) do
+      local WeaponSlot = DirectionalWeaponSlot[Octant]
+      if WeaponSlot then
+         local Weapons = WeaponsForSlot[WeaponSlot]
+         if not Weapons then
+            -- Lazy init
+            Weapons = GetWeaponsForSlot(Controllers, WeaponSlot)
+            WeaponsForSlot[WeaponSlot] = Weapons
+         end
+         -- Fire weapons
+         for _,Weapon in pairs(Weapons) do
+            -- Aim and fire each weapon. Don't really care what we aim at,
+            -- but it's necessary.
+            if Weapon.TurretIndex then
+               -- Turret mounted interceptors untested and most likely won't
+               -- work
+               I:AimWeaponInDirectionOnTurretOrSpinner(Weapon.TurretIndex, Weapon.Index, MissilePosition.x, MissilePosition.y, MissilePosition.z, WeaponSlot)
+               I:FireWeaponOnTurretOrSpinner(Weapon.TurretIndex, Weapon.Index, WeaponSlot)
+            else
+               I:AimWeaponInDirection(Weapon.Index, MissilePosition.x, MissilePosition.y, MissilePosition.z, WeaponSlot)
+               I:FireWeapon(Weapon.Index, WeaponSlot)
+            end
+         end
+      end
+   end
+end
+
+DirectionalIntercept = Periodic.create(UpdateRate, DirectionalIntercept_Update)
+
+function Update(I)
+   if not I:IsDocked() then
+      GetSelfInfo(I)
+
+      DirectionalIntercept:Tick(I)
+   end
+end
