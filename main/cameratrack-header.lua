551d8c6fa9157634726908deb9c2558a064e607b
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-10-08 17:23:06 -0700

    Add simple tracking script for cameras on turrets.

---
+++ main/cameratrack-header.lua
@@ -0,0 +1,9 @@
+-- CONFIGURATION
+
+-- How often to run. At 1, it will run every update. At 10,
+-- it will run every 10th update. The lower it is, the more
+-- responsive it will be, but it will also take more processing time.
+UpdateRate = 1
+
+-- Weapon slot of the turret that the camera is on
+CameraWeaponSlot = 1
---
+++ main/cameratrack.lua
@@ -0,0 +1,22 @@
+--! cameratrack
+--@ gettargetinfo getweaponcontrollers periodic
+function CameraTrack_Update(I)
+   if GetTargetInfo(I) then
+      local Turrets = GetWeaponControllers(I, TURRET)
+
+      for _,Weapon in pairs(Turrets) do
+         if Weapon.Slot == CameraWeaponSlot then
+            local Offset = TargetInfo.Position - Weapon.Position
+            I:AimWeaponInDirection(Weapon.Index, Offset.x, Offset.y, Offset.z, CameraWeaponSlot)
+         end
+      end
+   end
+end
+
+CameraTrack = Periodic.create(UpdateRate, CameraTrack_Update)
+
+function Update(I)
+   if not I:IsDocked() then
+      CameraTrack:Tick(I)
+   end
+end
