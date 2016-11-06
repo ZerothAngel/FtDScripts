26a463ff88dc4794761389f18d7fd759bd93662b
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-11-06 09:08:48 -0800

    Add subpump, a 3 degree-of-freedom air pump control script.

---
+++ main/subpump-footer.lua
@@ -0,0 +1,1 @@
+PumpType = AIRPUMP
---
+++ main/subpump.lua
@@ -0,0 +1,16 @@
+--! subpump
+--@ getselfinfo firstrun periodic
+--@ threedofpump depthcontrol
+ThreeDoFPump = Periodic.create(UpdateRate, Depth_Control)
+
+function Update(I) -- luacheck: ignore 131
+   if not I:IsDocked() then
+      GetSelfInfo(I)
+
+      if FirstRun then FirstRun(I) end
+
+      ThreeDoFPump:Tick(I)
+
+      ThreeDoFPump_Update(I)
+   end
+end
