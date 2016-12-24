6df0d7f34a910a9b90e1dea848e696543a6aac68
ZerothAngel <zerothangel@tyrannyofheaven.org>
2016-12-24 01:16:59 -0800

    rocketcontrol: Clean up & polish.

--- main/rocketcontrol.lua
+++ main/rocketcontrol.lua
@@ -3,17 +3,19 @@
 function RocketControl_Update(I)
    -- Get highest-priority non-salvage target
    local Target = C:FirstTarget()
-   if not Target then return end
-
-   for _,Weapon in pairs(C:HullWeaponControllers()) do
-      if Weapon.Slot == RocketWeaponSlot and (Weapon.Type == 4 or Weapon.Type == 5) then
-         -- Calculate aim point
-         local TargetVector = (Target.AimPoint - Weapon.Position).normalized
-         local AimPoint = QuadraticIntercept(Weapon.Position, TargetVector * RocketSpeed, Target.AimPoint, Target.Velocity, 9999)
-         -- Relative to weapon position
-         AimPoint = AimPoint - Weapon.Position
-         if I:AimWeaponInDirection(Weapon.Index, AimPoint.x, AimPoint.y, AimPoint.z, RocketWeaponSlot) > 0 then
-            I:FireWeapon(Weapon.Index, RocketWeaponSlot)
+   if Target then
+      -- Projectile velocity doesn't actually have to point anywhere
+      local Velocity = Vector3.forward * RocketSpeed
+      -- Aim & fire all turrets/missile controllers of the appropriate slot
+      for _,Weapon in pairs(C:HullWeaponControllers()) do
+         if Weapon.Slot == RocketWeaponSlot and (Weapon.Type == 4 or Weapon.Type == 5) and not Weapon.PlayerControl then
+            -- Calculate aim point
+            local AimPoint = QuadraticIntercept(Weapon.Position, Velocity, Target.AimPoint, Target.Velocity, 9999)
+            -- Relative to weapon position
+            AimPoint = AimPoint - Weapon.Position
+            if I:AimWeaponInDirection(Weapon.Index, AimPoint.x, AimPoint.y, AimPoint.z, RocketWeaponSlot) > 0 then
+               I:FireWeapon(Weapon.Index, RocketWeaponSlot)
+            end
          end
       end
    end
