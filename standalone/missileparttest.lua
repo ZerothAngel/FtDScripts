384622c9340c24143c4a2787ff136d8726e41c4b
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-03-31 11:10:01 -0700

    Simple script to get names of missile parts.

---
+++ standalone/missileparttest.lua
@@ -0,0 +1,15 @@
+local KeepGoing = true
+
+function Update(I)
+   if KeepGoing then
+      for tindex = 0,I:GetLuaTransceiverCount()-1 do
+         for mindex = 0,I:GetLuaControlledMissileCount(tindex)-1 do
+            local MissileInfo = I:GetMissileInfo(tindex, mindex)
+            for i,part in pairs(MissileInfo.Parts) do
+               I:Log(string.format("%d %s", i, part.Name))
+            end
+            KeepGoing = false
+         end
+      end
+   end
+end
