9be5bde84431586946b25f2570c885e98e0354eb
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-04-13 15:59:53 -0700

    yawthrottle: Rework to support yaw dediblade spinners.

--- control/yawthrottle-header.lua
+++ control/yawthrottle-header.lua
@@ -1,16 +1,21 @@
 -- YAW & PROPULSION
 
+-- Control fractions dedicated to spinners for each axis
+-- Set to a positive value up to and including 1 to use (dediblade)
+-- spinners for the given axis.
+YawThrottleSpinnerFractions = {
+   -- Side-facing spinners
+   Yaw = 0,
+   -- Forward- and reverse-facing spinners
+   Throttle = 0,
+}
+
 -- Yaw PID controller settings
 -- These default values have worked well for me on
 -- a variety of ships. YMMV.
 -- { 1, 0, 0 } is a good (but rough) starting point.
 YawPIDConfig = {
-   Kp = 0.25,
+   Kp = .25,
    Ti = 0,
-   Td = 0.4,
+   Td = .4,
 }
-
--- To use forward (or reverse!) spinners as propulsion,
--- set either or both of the following options to true.
-UseSpinners = false
-UseDediBlades = false
