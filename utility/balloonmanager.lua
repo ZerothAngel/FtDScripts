2dcc3db41f62d87792e85a8c8c4a00496adad57c
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-04-17 20:59:54 -0700

    balloonmanager: Just K.I.S.S. Keep no state.

--- utility/balloonmanager.lua
+++ utility/balloonmanager.lua
@@ -1,30 +1,17 @@
 --@ commons
 -- Balloon manager module
-BalloonManager_Active = false
-
 function BalloonManager_Update(I)
    local Altitude = C:Altitude()
    -- Always deploy when below deployment altitude
    if Altitude < BalloonManagerConfig.DeployBelow then
       I:DeployAllBalloons()
-   end
-   -- And only sever on transition from <= sever altitude to above
-   local SeverAbove = BalloonManagerConfig.SeverAbove
-   if Altitude <= SeverAbove then
-      BalloonManager_Active = true
-   elseif BalloonManager_Active and Altitude > SeverAbove then
+   elseif Altitude > BalloonManagerConfig.SeverAbove then
       I:SeverAllBalloons()
-      BalloonManager_Active = false
    end
 end
 
 function BalloonManager_Disable(I)
-   if BalloonManager_Active then
-      -- Sever once more, just in case
-      I:SeverAllBalloons()
-      -- And reset state
-      BalloonManager_Active = false
-   end
+   I:SeverAllBalloons()
 end
 
 function BalloonManager_Control(I)
