077d0be9798e9fba557412c052ea9a847d198e9f
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-08-15 11:19:11 -0700

    Start actively directing users away from unifiedmissile scripts.

--- main/udualprofilemain-header.lua
+++ main/udualprofilemain-header.lua
@@ -1,1 +1,6 @@
+-- Have a decent editor or are you somewhat familiar with coding? (Lua
+-- knowledge not necessary.) Have a look at my generalmissile-based
+-- scripts instead. Lots of options, lots of docs!
+-- https://tyrannyofheaven.org/ZerothAngel/public-ftd-scripts.html
+
 -- CONFIGURATION
--- main/unifiedmissilemain-header.lua
+++ main/unifiedmissilemain-header.lua
@@ -1,3 +1,8 @@
+-- Have a decent editor or are you somewhat familiar with coding? (Lua
+-- knowledge not necessary.) Have a look at my generalmissile-based
+-- scripts instead. Lots of options, lots of docs!
+-- https://tyrannyofheaven.org/ZerothAngel/public-ftd-scripts.html
+
 -- CONFIGURATION
 
 -- Generally these should match the settings on your Local Weapon Controller.
