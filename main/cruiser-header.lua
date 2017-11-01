01c127dc789526bf7933cabc20bc3cc43a3c6dfd
ZerothAngel <zerothangel@tyrannyofheaven.org>
2017-11-01 10:30:11 -0700

    Add cruiser combo script, a more generalized warship script.

---
+++ main/cruiser-header.lua
@@ -0,0 +1,28 @@
+-- Meant for naval surface vehicles with yaw + propulsion control.
+-- Destroyer/cruiser variant (cannons + missiles) with (optional) hydrofoil
+-- control of depth, pitch, roll. May also carry auxiliary craft.
+
+-- CONFIGURATION
+
+-- Activate on these AI modes. Valid keys are "off", "on", "combat",
+-- "patrol", and "fleetmove".
+ActivateWhen = {
+--   on = true,
+   combat = true,
+   fleetmove = true,
+}
+
+-- How often to run. At 1, it will run every update. At 10,
+-- it will run every 10th update. The lower it is, the more
+-- responsive it will be, but it will also take more processing time.
+-- Set to nil to disable a module.
+AI_UpdateRate = 4
+Cannon_UpdateRate = 1
+Missile_UpdateRate = 4
+ShieldManager_UpdateRate = nil
+DockManager_UpdateRate = nil
+
+-- Fixed altitude, measured from CoM (negative for below sea level).
+-- Only used when controlling altitude in some way.
+-- (see CONTROL CONFIGURATION and HYDROFOIL CONFIGURATION)
+FixedAltitude = 0
---
+++ main/cruiser.lua
@@ -0,0 +1,63 @@
+--! cruiser
+--@ commons control firstrun periodic
+--@ shieldmanager dockmanager multiprofile cannoncontrol rollturn subcontrol sixdof ytdefaults naval-ai
+-- Cruiser main
+DockManager = Periodic.new(DockManager_UpdateRate, DockManager_Update, 3)
+ShieldManager = Periodic.new(ShieldManager_UpdateRate, ShieldManager_Control, 2)
+MissileMain = Periodic.new(Missile_UpdateRate, MissileMain_Update, 1)
+Cannon = Periodic.new(Cannon_UpdateRate, CannonControl_Update)
+NavalAI = Periodic.new(AI_UpdateRate, NavalAI_Update)
+
+-- Set up a hybrid control system for altitude-pitch-roll
+Hybrid = {}
+function Hybrid.SetAltitude(Alt)
+   SubControl.SetAltitude(Alt)
+   SixDoF.SetAltitude(Alt)
+end
+function Hybrid.SetPitch(Angle)
+   SubControl.SetPitch(Angle)
+   SixDoF.SetPitch(Angle)
+end
+function Hybrid.SetRoll(Angle)
+   SubControl.SetRoll(Angle)
+   SixDoF.SetRoll(Angle)
+end
+
+SelectHeadingImpl(SixDoF, RollTurnControl)
+SelectRollImpl(Hybrid, RollTurnControl)
+
+SelectHeadingImpl(RollTurn)
+SelectThrottleImpl(SixDoF)
+SelectAltitudeImpl(Hybrid)
+SelectPitchImpl(Hybrid)
+SelectRollImpl(RollTurn)
+
+function Update(I) -- luacheck: ignore 131
+   C = Commons.new(I)
+   if FirstRun then FirstRun(I) end
+   if not C:IsDocked() then
+      if ActivateWhen[I.AIMode] then
+         NavalAI:Tick(I)
+
+         -- Suppress default AI
+         I:TellAiThatWeAreTakingControl()
+      else
+         NavalAI_Reset()
+         V.Reset()
+         SixDoF.Release(I)
+      end
+
+      V.SetAltitude(FixedAltitude)
+      SubControl.Update(I)
+      SixDoF.Update(I)
+
+      Cannon:Tick(I)
+      MissileMain:Tick(I)
+   else
+      NavalAI_Reset()
+      SixDoF.Disable(I)
+   end
+
+   DockManager:Tick(I)
+   ShieldManager:Tick(I)
+end
