3f051c5c0a33c06750cd5288a40b3dbfc3df35b8
ZerothAngel <zerothangel@tyrannyofheaven.org>
2018-03-27 22:27:27 -0700

    Add plane-like submarine script.

---
+++ main/planelikesub-footer.lua
@@ -0,0 +1,5 @@
+-- Control mode. Set to WATER, LAND, or AIR.
+Mode = WATER
+
+-- Max altitude for interpolation of pitch table
+PlaneLike_MaxAltitude = 500
---
+++ main/planelikesub.lua
@@ -0,0 +1,67 @@
+--! planelikesub
+--@ commons control firstrun periodic
+--@ shieldmanager multiprofile subcontrol sixdof planelike planelikedefaults depthcontrol naval-ai
+-- Plane-like sub main
+ShieldManager = Periodic.new(ShieldManager_UpdateRate, ShieldManager_Control, 3)
+MissileMain = Periodic.new(Missile_UpdateRate, MissileMain_Update, 2)
+DepthControl = Periodic.new(DepthControl_UpdateRate, Depth_Control, 1)
+NavalAI = Periodic.new(AI_UpdateRate, NavalAI_Update)
+
+-- Set up a hybrid control system for altitude-pitch-roll
+Hybrid = {}
+function Hybrid.SetAltitude(Alt)
+   SubControl.SetAltitude(Alt)
+   SixDoF.SetAltitude(Alt)
+   PlaneLike.SetAltitude(Alt)
+end
+function Hybrid.SetPitch(Angle)
+   SubControl.SetPitch(Angle)
+   SixDoF.SetPitch(Angle)
+end
+function Hybrid.SetRoll(Angle)
+   SubControl.SetRoll(Angle)
+   SixDoF.SetRoll(Angle)
+end
+
+SelectHeadingImpl(SixDoF, PlaneLikeControl)
+SelectPitchImpl(Hybrid, PlaneLikeControl)
+SelectRollImpl(Hybrid, PlaneLikeControl)
+
+SelectHeadingImpl(PlaneLike)
+SelectThrottleImpl(SixDoF)
+SelectAltitudeImpl(Hybrid)
+
+function Update(I) -- luacheck: ignore 131
+   C = Commons.new(I)
+   FirstRun(I)
+   if not C:IsDocked() then
+      if ActivateWhen[I.AIMode] then
+         -- Note that the airplane module is wholly dependent on
+         -- the AI, so DepthControl and PlaneLike.Update
+         -- have been moved here.
+         DepthControl:Tick(I)
+
+         NavalAI:Tick(I)
+
+         -- Suppress default AI
+         I:TellAiThatWeAreTakingControl()
+
+         Depth_Apply(I, DodgeAltitudeOffset)
+         PlaneLike.Update(I)
+      else
+         NavalAI_Reset()
+         V.Reset()
+         SixDoF.Release(I)
+      end
+
+      SubControl.Update(I)
+      SixDoF.Update(I)
+
+      MissileMain:Tick(I)
+   else
+      NavalAI_Reset()
+      SixDoF.Disable(I)
+   end
+
+   ShieldManager:Tick(I)
+end
